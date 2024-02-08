3310890e6f2b304bc37bd908473f62749bf27e19
Enriquecarvajalnunez <enrique.carvajal.nunez@gmail.com>
2024-02-08 16:16:14 -0500

    upload
    se modifica ciclo foreach

--- app/Upload.php
+++ app/Upload.php
@@ -2,19 +2,17 @@
 namespace App;
 
 use Illuminate\Support\Facades\Storage;
-use PhpOffice\PhpSpreadsheet\IOFactory;//IOFactory adivina el tipo de plantilla con la que se trabaja
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Upload extends Model
 {
                 
     public function __construct($rutaArchivo, $filename, $options)
-    {     
-
+    {             
+        
         //aqui en options lo recorre con un foreach y dependiendo si es mar, currier se realiza la accion necesaria
-
         $rutaArchivo = storage_path('app/' . $rutaArchivo);
 
         // Cargas el archivo desde el sistema de archivos de Laravel con PhpSpreadsheet
@@ -26,32 +24,30 @@ class Upload extends Model
 
         $celda = '';
         $int = 1;
-        
-        foreach($worksheet->getRowIterator(2) as $row)//iterados de filas comienza en fila 2    
-        {   
-            
-            $cellIterator = $row->getCellIterator();//Iterador de celdas recorre las celdas                                              
-                                                                                            
-            /*Imprime contenido de celdas*/
-            $i=1; //itera entre columnas en excel inicia desde el indice 1, variable bandera
-            foreach ($cellIterator as $cell) //columnas
-            {             
-            
-            if($i==1)
-                {                        
-                    $Header.= str_pad('P0'.$cell->getValue(),18,chr(32),STR_PAD_RIGHT); //obtiene valor de la celda                
-                    $i++;
-                
-                }else
-                {
-                    $Header.=str_pad($cell->getValue(), 4, '0', STR_PAD_LEFT) .$this->espacios(8); //obtiene valor de la celda                                     
-                }                         
-            }
-            
-        }
 
+        if($options[0] == "currier")
+        {
+            foreach($worksheet->getRowIterator(2) as $row)//iterados de filas comienza en fila 2    
+            {               
+                $cellIterator = $row->getCellIterator();//Iterador de celdas recorre las celdas                                                                                                                                              
+                /*Imprime contenido de celdas*/
+                $i=1; //itera entre columnas en excel inicia desde el indice 1, variable bandera
+                foreach ($cellIterator as $cell) //columnas
+                {                             
+                    if($i==1)
+                    {                        
+                            $Header.= str_pad('P0'.$cell->getValue(),18,chr(32),STR_PAD_RIGHT); //obtiene valor de la celda                
+                            $i++;                        
+                    }else
+                        {
+                            $Header.=str_pad($cell->getValue(), 4, '0', STR_PAD_LEFT) .$this->espacios(8); //obtiene valor de la celda                                     
+                        }                         
+                }            
+            }            
+        }
+                
         $Header.= $Piepagina;        
-        $report_output = 'C:/laragon/www/uploadXMLProcess/public/archivos/'.$filename.'.txt';
+        $report_output = 'C:/inetpub/wwwroot/uploadXMLProcess/storage/app/archivos'.$filename.'.txt';
         file_put_contents($report_output,$Header);  
         
         echo '<script language="javascript">alert("Proceso terminado satisfactoriamente !!");</script>';
